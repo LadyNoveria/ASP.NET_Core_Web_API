4bb33208d604088f892d0e6156ca138094c33366
LadyNoveria <48456320+LadyNoveria@users.noreply.github.com>
2021-04-25 02:04:51 +0300

    описаны get, put, post, delete.
    delete пока не работает

---
+++ HW_1/HW_1/KeeperOfWeatherForecasts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW_1
+{
+    public class KeeperOfWeatherForecasts
+    {
+        public List<WeatherForecast> WeatherForecasts { get; set; }
+        public KeeperOfWeatherForecasts()
+        {
+            WeatherForecasts = new List<WeatherForecast>();
+        }
+    }
+}
