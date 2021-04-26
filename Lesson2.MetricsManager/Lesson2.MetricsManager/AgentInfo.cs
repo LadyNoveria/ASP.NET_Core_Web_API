5e157b7a8fc5542ef659595105a208f7f8baafc4
LadyNoveria <48456320+LadyNoveria@users.noreply.github.com>
2021-04-27 00:38:41 +0300

    добавлен проект metricsmanager
    добавлены контроллеры для cpu, ram, hdd, network и dotnetб добавлен контроллер agentmanager. Логика пока не проверена.

---
+++ Lesson2.MetricsManager/Lesson2.MetricsManager/AgentInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson2.MetricsManager
+{
+    public class AgentInfo
+    {
+        public int AgentId { get; }
+        public Uri AgentAddress { get; } 
+        public AgentInfo(int agentId, Uri agentAddress)
+        {
+            AgentId = agentId;
+            AgentAddress = agentAddress;
+        }
+    }
+}
