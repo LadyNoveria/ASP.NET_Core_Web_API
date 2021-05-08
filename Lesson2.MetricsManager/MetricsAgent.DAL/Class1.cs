3f7baf140dcee4bdac80c0f91e7232b42e4b537e
LadyNoveria <48456320+LadyNoveria@users.noreply.github.com>
2021-05-09 01:13:44 +0300

    отладка

---
+++ Lesson2.MetricsManager/MetricsAgent.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MetricsAgent.DAL
+{
+    public class Class1
+    {
+    }
+}
--- Lesson2.MetricsManager/MetricsAgent/Controllers/CpuMetricsController.cs
+++ Lesson2.MetricsManager/MetricsAgent/Controllers/CpuMetricsController.cs
@@ -4,6 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using MetricsAgent.Requests;
+using MetricsAgent.Responses;
+using Microsoft.Extensions.Logging;
+using MetricsAgent.Repositories;
 
 namespace MetricsAgent.Controllers
 {
@@ -11,10 +16,48 @@ namespace MetricsAgent.Controllers
     [ApiController]
     public class CpuMetricsController : ControllerBase
     {
-        [HttpGet("from/{fromTime}/to/{toTime}")]
-        public IActionResult GetInfo([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        private readonly ILogger<CpuMetricsController> _logger;
+        private ICpuMetricsRepository _repository;
+        public CpuMetricsController(ICpuMetricsRepository repository)
         {
+            _repository = repository;
+        }
+        public CpuMetricsController(
+            ICpuMetricsRepository repository, 
+            ILogger<CpuMetricsController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] CpuMetricCreateRequest request)
+        {
+            _logger.LogInformation("api/metrics/cpu/Create");
+            _repository.Create(new CpuMetric
+            {
+                Time = request.Time,
+                Value = request.Value
+            }) ;
             return Ok();
+        }
+
+        [HttpGet("from/{fromTime}/to/{toTime}")]
+        public IActionResult GetByTimePeriod(
+            [FromRoute] TimeSpan fromTime, 
+            [FromRoute] TimeSpan toTime)
+        {
+            _logger.LogInformation("api/metrics/cpu/GetByTimePeriod");
+            var metrics = _repository.GetAll();
+            var response = new AllCpuMetricsResponse() { Metrics = new List<CpuMetricDto>()};
+            foreach (var metric in metrics)
+            {
+                response.Metrics.Add(new CpuMetricDto { 
+                    Time = metric.Time, 
+                    Value = metric.Value, 
+                    Id = metric.Id}); 
+            }
+            return Ok(response);
         }
     }
 }
